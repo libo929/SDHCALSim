fedb28eda313633473bbc127748991c5256a8f3c
Bo Li <libo0929@gmail.com>
2020-02-19 16:51:15 +0800

    Added the cpp file for UI mode.

---
+++ exec/SDHCALSimUI.cxx
@@ -0,0 +1,129 @@
+#include "SDHCALDetectorConstruction.h"
+#include "SDHCALPrimaryGeneratorAction.h"
+#include "SDHCALRunAction.h"
+#include "SDHCALEventAction.h"
+#include "SDHCALTrackingAction.h"
+#include "SDHCALStackingAction.h"
+
+#include <G4PhysListFactory.hh>
+
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+#ifdef G4UI_USE
+#include "G4UIExecutive.hh"
+#endif
+
+#include <string>
+
+#include "json.hpp"
+
+
+struct Params
+{
+		G4int seed = 1 ;
+		G4int nEvent = 1 ;
+		G4String physicsList = "FTFP_BERT" ;
+		G4String outputFileName = "output" ;
+		G4bool killNeutrons = false ;
+} ;
+
+Params readJsonFile(G4String jsonFileName)
+{
+	Params params ;
+
+	if ( jsonFileName == G4String("") )
+	{
+		std::cout << "ERROR : no json file provided" << std::endl ;
+		std::terminate() ;
+	}
+
+	std::ifstream file(jsonFileName) ;
+	auto json = nlohmann::json::parse(file) ;
+
+	if ( json.count("outputFileName") )
+		params.outputFileName = json.at("outputFileName").get<G4String>() ;
+	if ( json.count("physicsList") )
+		params.physicsList = json.at("physicsList").get<G4String>() ;
+	if ( json.count("nEvents") )
+		params.nEvent = json.at("nEvents").get<G4int>() ;
+	if ( json.count("seed") )
+		params.seed = json.at("seed").get<G4int>()  + 1 ;
+	if ( json.count("killNeutrons") )
+		params.killNeutrons = json.at("killNeutrons").get<G4bool>() ;
+
+	return params ;
+}
+
+int main(int argc , char** argv)
+{
+	if ( argc != 2 )
+	{
+		G4cerr << "Error with arguments passed for the program" << G4endl ;
+		std::terminate() ;
+	}
+	
+	G4String jsonFileName = argv[1] ;
+
+	//Params params = readJsonFile( jsonFileName ) ;
+	Params params;
+
+	CLHEP::HepRandom::setTheSeed(params.seed) ;
+
+	G4RunManager* runManager = new G4RunManager ;
+
+	// Detector construction
+	runManager->SetUserInitialization( new SDHCALDetectorConstruction(jsonFileName) ) ;
+
+	// Physics list
+	G4PhysListFactory* physFactory = new G4PhysListFactory() ;
+	runManager->SetUserInitialization( physFactory->GetReferencePhysList(params.physicsList) ) ;
+
+	// Primary generator action
+	runManager->SetUserAction( new SDHCALPrimaryGeneratorAction( jsonFileName ) ) ;
+
+	SDHCALRunAction* runAction = new SDHCALRunAction() ;
+
+	runAction->setLcioFileName( params.outputFileName + G4String(".slcio") ) ;
+	runAction->setRootFileName( params.outputFileName + G4String(".root") ) ;
+
+	runManager->SetUserAction( runAction ) ;
+	runManager->SetUserAction( new SDHCALEventAction(runAction) ) ;
+
+	runManager->SetUserAction( SDHCALSteppingAction::Instance() ) ;
+	runManager->SetUserAction( SDHCALTrackingAction::Instance() ) ;
+	runManager->SetUserAction( SDHCALStackingAction::Instance() ) ;
+
+	SDHCALStackingAction::Instance()->setKillNeutrons(params.killNeutrons) ;
+
+	runManager->Initialize() ;
+
+	G4VisManager* visManager = new G4VisExecutive("Quiet");
+	visManager->Initialize();
+
+	G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+
+	G4UImanager::GetUIpointer()->ApplyCommand("/control/macroPath /Users/libo/workplace/SDHCALSim/script");
+	G4UImanager::GetUIpointer()->ApplyCommand("/control/execute init_vis.mac");
+
+	if (ui->IsGUI()) {
+		G4UImanager::GetUIpointer()->ApplyCommand("/control/execute gui.mac");
+	}
+
+	ui->SessionStart();
+
+	//runManager->BeamOn(params.nEvent) ;
+
+	delete ui;
+	delete visManager;
+	delete runManager;
+
+	return 0 ;
+}
